bfe94817ca1e52375e721396d53e7d40236b7ba9
ruijosecj <ruijosecj@gmail.com>
2023-05-30 19:52:53 -0300

    Nao permite adicionar numero repetido e numero fora do formato

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 const form = document.getElementById('form-contato')
-const nome = document.getElementById('nome-contato')
-const telefone = document.getElementById('numero-contato')
-let linhas = '';
+let linhas =''
+let fones = []
+
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
-
-
-    let linha = `<tr>`;
-    linha+= `<td>${nome.value}</td>`;
-    linha+= `<td>${telefone.value}</td>`;
-    linha+= `</tr>`;
-
-    linhas+= linha
-
-    const corpoTabela = document.querySelector('tbody');
-
-    corpoTabela.innerHTML=linhas;
-
-    nome.value='';
-    telefone.value='';
+    
+    addLinhas()
+    renderTbody()
+    limpaInput()
+    
 })
+
+function addLinhas(){
+    const inputNome = document.getElementById('inputNome')
+    const inputFone = document.getElementById('inputFone')
+    
+    if(fones.includes(inputFone.value)){
+        alert(' Número de telefone já existe')
+    }else{
+        fones.push(inputFone.value)
+
+        let linha = `<tr>`;
+        linha+= `<td>${inputNome.value}</td>`;
+        linha+= `<td>${inputFone.value}</td>`;
+        linha+= `</tr>`;
+    
+        linhas+= linha
+    }
+
+}
+
+function renderTbody(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function limpaInput(){
+    inputNome.value='';
+    inputFone.value='';
+    inputNome.focus();
+}
